d4b85c033f83cf0a11e24862498114f7cf3bbf38
Denis Romanenko <deromanenko@gmail.com>
2023-02-13 20:43:32 +0300

    feat: Category and Post models/migrations

---
+++ database/migrations/2023_02_13_164429_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            // Я не знаю, как называется часть статьи, которая
+            // идёт до ката. Пусть будет before_cut
+            $table->string('before_cut', 100);
+            $table->text('body');
+            $table->foreignId('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_02_13_164436_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            // Чтобы категории веселее смотрелись, добавим им эмоджи
+            // Дефолтным сделаем 'under construction'
+            $table->string('emoji', 16)->default('&#128679;');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
