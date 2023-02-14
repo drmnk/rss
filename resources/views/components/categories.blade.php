771c6c61014809efda529e0f3759725fbbaa3de0
Denis Romanenko <deromanenko@gmail.com>
2023-02-14 11:38:38 +0300

    Fix problem with incorrect higlighting of active category

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -2,14 +2,15 @@
     @foreach ($categories as $category)
         <a href="{{ route('posts.category', $category->slug) }}">
             <div
-                class="bg-slate-600 shadow-xl p-2 rounded-md font-semibold text-white
-        @isset($currentCategory)
-            @if ($category->id === $currentCategory->id)
-                bg-orange-500
-
-            @endif
-        @endisset
-    ">
+                class="shadow-xl p-2 rounded-md font-semibold text-white
+                @isset($currentCategory)
+                    @if ($category->id === $currentCategory->id)
+                        bg-orange-500
+                    @else
+                        bg-slate-600 
+                    @endif
+                @endisset
+            ">
                 {{ $category->emoji }} {{ $category->name }}
             </div>
         </a>
