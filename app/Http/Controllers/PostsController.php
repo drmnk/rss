9aee1b6e34983524969e53ade989e7dc0d5836b2
Denis Romanenko <deromanenko@gmail.com>
2023-02-14 11:49:52 +0300

    Post model with scope

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Scopes\PostNoBodyScope;
 
 class PostsController extends Controller
 {
@@ -20,6 +21,14 @@ class PostsController extends Controller
         $categories = Category::all();
         $currentCategory = Category::where('slug', $categorySlug)->first();
         $posts = $currentCategory->posts()->get()->map(function ($post) use ($currentCategory) {
+            /*
+                Для корректного отображения в одном и том же виде, нам нужно,
+                чтобы у поста была его родительская категория
+                Не то чтобы этот map обязателен, и мы легко могли бы получить посты
+                с их родительской категорией как
+                $posts = $currentCategory->posts()->with('category')->get();
+                Но это позволит нам сэкономить одно обращение к БД
+            */
             $post->category = $currentCategory;
 
             return $post;
@@ -32,5 +41,8 @@ class PostsController extends Controller
     {
         $categories = Category::all();
         $currentCategory = Category::where('slug', $categorySlug)->first();
+        $post = Post::withoutGlobalScope(PostNoBodyScope::class)->find($id);
+
+        return view('post', compact('categories', 'currentCategory', 'post'));
     }
 }
